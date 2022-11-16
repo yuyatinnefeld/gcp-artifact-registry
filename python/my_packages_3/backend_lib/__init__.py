3242837dc27102eebf9e637f8b093bbe54f193c8
yuyatinnefeld <yuyawamo@gmail.com>
2022-11-16 07:49:15 +0100

    feat: update test stage and add utils.py for packages

---
+++ python/my_packages_3/backend_lib/__init__.py
@@ -0,0 +1,1 @@
+from .utils import backendend_module_1, backendend_module_2
---
+++ python/run_frontend_packages.py
@@ -0,0 +1,11 @@
+############### TEST My Packages 2 ###############
+
+print(f"{'#'*10} TEST - My Packages 2 {'#'*10}")
+
+import frontend_lib
+
+print(f"{'*'*5} TEST - Frontend Module 1 {'*'*5}")
+frontend_lib.frontend_module_1()
+
+print(f"{'*'*5} TEST - Frontend Module 2 {'*'*5}")
+frontend_lib.frontend_module_2()
